3c40ba454de8a2bb7290fa94ca4b619dd80fa455
Xavier Garcia <xmangarcia17@gmail.com>
2025-03-16 23:39:31 -0700

    Initial commit

---
+++ app/src/main/java/com/zybooks/petadoption/ui/AdoptViewModel.kt
@@ -0,0 +1,9 @@
+package com.zybooks.petadoption.ui
+
+import androidx.lifecycle.ViewModel
+import com.zybooks.petadoption.data.Pet
+import com.zybooks.petadoption.data.PetDataSource
+
+class AdoptViewModel : ViewModel() {
+    fun getPet(id: Int): Pet = PetDataSource().getPet(id) ?: Pet()
+}
---
+++ app/src/main/java/com/zybooks/petadoption/ui/ListViewModel.kt
@@ -0,0 +1,8 @@
+package com.zybooks.petadoption.ui
+
+import androidx.lifecycle.ViewModel
+import com.zybooks.petadoption.data.PetDataSource
+
+class ListViewModel : ViewModel() {
+    val petList = PetDataSource().loadPets()
+}
